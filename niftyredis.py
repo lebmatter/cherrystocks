b19324a8e346d9084f93b922e18e37dd76c1c0df
Labeeb Mattra <mmlabeeb@gmail.com>
2016-11-29 19:16:59 +0530

    redis functions

---
+++ niftyredis.py
@@ -0,0 +1,53 @@
+import redis
+from niftyparser import get_niftyfifty
+import datetime
+import time
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def get_or_update():
+    '''
+    Funtion checks for avialable keys in keyspace.
+    1) If key not found:
+        get data from NSE website.
+        Save the values with timestamp as key and ttl 5 minutes.
+    2) If key is found:
+        get data from cache with timestamp Key
+    '''
+    keys = r.keys()
+    if not keys:
+        # empty keys means cache has cleared.
+        data = get_niftyfifty()
+        tstamp = datetime.datetime.strptime(data['time'], "%b %d, %Y %H:%M:%S")
+        tstamp = int(time.mktime(tstamp.timetuple()))
+        # Save timestamp for reference
+        r.set('tstamp', tstamp)
+        r.expire('tstamp', 300)
+        for entry in data['data']:
+            entry_key = '{}:{}'.format(tstamp, entry['symbol'])
+            r.hmset(
+                entry_key, {
+                    'symbol': entry_key,
+                    'openPrice': entry['openPrice'],
+                    'highPrice': entry['highPrice'],
+                    'lowPrice': entry['lowPrice'],
+                    'ltp': entry['ltp'],
+                    'previousPrice': entry['previousPrice'],
+                    'netPrice': entry['netPrice'],
+                    'tradedQuantity': entry['tradedQuantity'],
+                    'turnoverInLakhs': entry['turnoverInLakhs']})
+
+            r.expire(entry_key, 300)
+        return data['data']
+    elif 'tstsamp' in keys:
+        last_tstamp = r.get('tstamp')
+        entry_keys = keys.remove(last_tstamp)
+        data = []
+        for entry_key in entry_keys:
+            data.append(r.hgetall(entry_key))
+        return data
+
+
+if __name__ == '__main__':
+    data = get_niftyfifty()
+    print data
