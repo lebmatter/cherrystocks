6b1d66a096c7ab0c1e92a39f8678435189b0ba09
Labeeb Mattra <mmlabeeb@gmail.com>
2016-11-29 19:16:09 +0530

    Ajax get function added

--- cherrystocks.py
+++ cherrystocks.py
@@ -1,11 +1,20 @@
 import cherrypy
 import os
+from niftyredis import get_or_update
 
 
 class CherryStocks(object):
     @cherrypy.expose
     def index(self):
-        return "Hello!"
+        return open('index.html')
+
+@cherrypy.expose
+class CherryStocksWebService(object):
+
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self):
+        data = get_or_update()
+        return data
 
 
 if __name__ == '__main__':
@@ -13,9 +22,17 @@ if __name__ == '__main__':
         '/': {
             'tools.staticdir.root': os.path.abspath(os.getcwd())
         },
+        '/niftyfifty': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
         '/static': {
             'tools.staticdir.on': True,
             'tools.staticdir.dir': './public'
         }
     }
-    cherrypy.quickstart(CherryStocks(), '/', conf)
+
+    webapp = CherryStocks()
+    webapp.niftyfifty = CherryStocksWebService()
+    cherrypy.quickstart(webapp, '/', conf)
