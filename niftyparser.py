e3f9725a3b95dbc5c4f0a66afaff6dcb52609e59
Labeeb Mattra <mmlabeeb@gmail.com>
2016-11-29 12:43:54 +0530

    Json parser for nifty 50 data

---
+++ niftyparser.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+def get_niftyfifty():
+    '''
+    I wanted to use BeutifulSoup at first. But it won't parse dynamic data.
+    Then next option was Selenium with PhantomJS, whch I was familiar with. But it will be an overhead.
+    So decided to find the ajax call and use it to get 'Nifty 50' data.'''
+
+    url = 'https://www.nseindia.com/live_market/dynaContent/live_analysis/gainers/niftyGainers1.json'
+    req = requests.get(url)
+
+    data = json.loads(req.text)
+    return data
+
+
+if __name__ == '__main__':
+    data = get_niftyfifty()
+    print data
