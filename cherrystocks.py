04bc7d4a14f2f0d8bebca9f851398a99c99f96ac
Labeeb Mattra <mmlabeeb@gmail.com>
2016-11-30 12:27:54 +0530

    config update

--- cherrystocks.py
+++ cherrystocks.py
@@ -33,6 +33,9 @@ if __name__ == '__main__':
         }
     }
 
+    cherrypy.config.update({'server.socket_host': '0.0.0.0',
+                        'server.socket_port': 8080,
+                       })
     webapp = CherryStocks()
     webapp.niftyfifty = CherryStocksWebService()
     cherrypy.quickstart(webapp, '/', conf)
