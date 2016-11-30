731edcf3f036c3eb113e5cbd8684bab3175dc72f
Labeeb Mattra <mmlabeeb@gmail.com>
2016-12-01 03:23:36 +0530

    huey changes

--- tasks.py
+++ tasks.py
@@ -1,8 +1,18 @@
 from huey import RedisHuey, crontab
 from niftyredis import get_or_update
 
-huey = RedisHuey('niftyupdater', host='localhost:6379/0')
+huey = RedisHuey()
+
 
 @huey.periodic_task(crontab(minute='5', hour='0'))
 def periodic_crawler():
+    '''
+    Huey task scheduler is simpler to use.
+    Other options I checked were:
+    1) threading.Timer() in a loop
+    2) Celelry with Redis backed.
+
+    To run the scheduler in a different thread,
+
+    '''
     get_or_update()
